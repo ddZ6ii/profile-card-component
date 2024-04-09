c7cd979cf9dc117bc342802a49bd44cade5908f2
Denis Dezest <denis.dezest@gmail.com>
2024-04-10 01:21:04 +0200

    🎉 Init repo

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5';
+
+// Define chrome as the default browser for the dev server
+const opsys = process.platform;
+// windows
+if (opsys === 'win32' || opsys === 'win64') process.env.BROWSER = 'chrome';
+// macOS
+if (opsys === 'darwin') process.env.BROWSER = '/Applications/Google Chrome.app';
+
+export default defineConfig({
+  root: './src',
+  publicDir: '../public',
+  build: {
+    emptyOutDir: true,
+    outDir: '../dist',
+    sourcemap: true,
+  },
+  plugins: [
+    // HTML minification
+    ViteMinifyPlugin({
+      removeComments: true,
+    }),
+    // Purge CSS: remove unused CSS class
+    pluginPurgeCss({
+      content: ['./src/**/*.html'],
+      sourceMap: true,
+      variables: true,
+    }),
+  ],
+});
